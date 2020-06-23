993dd8af0d38aef2bd3f7ffd6bd1d5dd26a36b3f
S. Otzoy <otzoy97@hotmail.com>
2020-06-23 02:33:44 -0600

    some tokens were added

--- analyzer/lexer.py
+++ analyzer/lexer.py
@@ -18,9 +18,8 @@ reserved = {
     'default': 'DEFAULT',
     'return': 'RETURN',
     'sizeof': 'SIZEOF',
-    #'auto': 'AUTO',
-    #'const': 'CONST',
-    'cast': 'CAST'
+    'true': 'TRUE',
+    'false': 'FALSE'
 }
 
 tokens = [
@@ -28,26 +27,121 @@ tokens = [
     'FLOATVAL',
     'STRVAL',
     'CHARVAL',
-    'ID'
+    'ID',
+    'CORL',
+    'CORR',
+    'PARR',
+    'PARL',
+    #'LLVR',
+    #'LLVL',
+    'DOT',
+    'MINUS',
+    'PLUS',
+    'TIMES',
+    'QUOT',
+    'REM',
+    'NOT',
+    'AND',
+    'OR',
+    'NOTBW',
+    'ANDBW',
+    'ORBW',
+    'XORBW',
+    'SHL',
+    'SHR',
+    'EQ',
+    'ASSIGN',
+    'NEQ',
+    'GR',
+    'GRE',
+    'LS',
+    'LSE',
+    'INC',
+    'DEC',
+    'TIMESA',
+    'QUOTA',
+    'REMA',
+    'PLUSA',
+    'MINUSA',
+    'SHRA',
+    'SHLA',
+    'ANDBWA',
+    'XORBWA',
+    'ORBWA',
+    'QUESR',
+    'COLON',
+    'COMMA'
 ] + list(reserved.values())
 
-def t_FLOATVAL(self,t):
+t_DOT           = r'\.'
+t_CORL          = r'\['
+t_CORR          = r'\]'
+t_PARL          = r'\('
+t_PARR          = r'\)'
+#t_LLVL          = r'\{'
+#t_LLVR          = r'\}'
+t_MINUS         = r'-'
+t_PLUS          = r'\+'
+t_TIMES         = r'\*'
+t_QUOT          = r'/'
+t_REM           = r'%'
+t_NOT           = r'!'
+t_AND           = r'&&'
+t_OR            = r'\|\|'
+t_NOTBW         = r'~'
+t_ANDBW         = r'&'
+t_ORBW          = r'\|'
+t_XORBW         = r'\^'
+t_SHL           = r'<<'
+t_SHR           = r'>>'
+t_EQ            = r'=='
+t_ASSIGN        = r'='
+t_NEQ           = r'!='
+t_GR            = r'>'
+t_GRE           = r'>='
+t_LS            = r'<'
+t_LSE           = r'<='
+t_DEC           = r'\+\+'
+t_INC           = r'--'
+t_TIMESA        = r'\*='
+t_QUOTA         = r'/='
+t_REMA          = r'%='
+t_PLUSA         = r'\+='
+t_MINUSA        = r'-='
+t_SHRA          = r'>>='
+t_SHLA          = r'<<='
+t_ANDBWA        = r'&='
+t_XORBWA        = r'\^='
+t_ORBWA         = r'\|='
+t_QUESR         = r'\?'
+t_COLON         = r'\:'
+t_COMMA         = r'\,'
+
+def t_FLOATVAL(t):
     r'\d+\.\d+'
     try:
         t.value = float(t.value)
     except:
+        #TODO: this is an error: this is not a number
         t.value = 0.0
     return t
 
-def t_INTVAL(self,t):
+def t_INTVAL(t):
     r'\d+'
     try:
         t.value = int(t.value)
-    except:
-        t.value = 0
+    except ValueError: 
+        #TODO: this is an error: overflow int data
+        # implicit cast
+        try:
+            t.value = float(t.value)
+            t.type = 'FLOATVAL'
+        except:
+            #TODO: this is an error: this is not a number
+            t.value = 0
     return t
 
-def t_STRVAL(self,t):
+def t_STRVAL(t):
     r'\"[^\"\\]*(\\.[^\"\\]*)*\"'
     #replace \\ with \
     str_ = t.value[1:-1]
@@ -56,29 +150,29 @@ def t_STRVAL(self,t):
     #str_ = str_.replace("\\t","\t")
     t.value = str_
 
-def t_CHARVAL(self,t):
+def t_CHARVAL(t):
     r"'((\\.)|([^'\\]))'"
     t.value = t.value[1:-1]
 
-def t_ID(self,t):
+def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     t.type = reserved.get(t.value, 'ID')
 
-def t_COMMENT(self,t):
+def t_COMMENT(t):
     r'\#.*\n*?'
     t.lexer.lineno += 1
 
-def t_MCOMMENT(self,t):
+def t_MCOMMENT(t):
     r'/\*(.|\n)*?\*/'
     t.lexer.lineno += t.value.count('\n')
 
 t_ignore = " \t"
 
-def t_newline(self,t):
+def t_newline(t):
     r'\n+'
     t.lexer.lineno += len("\n")
 
-def t_error(self,t):
+def t_error(t):
     t.lexer.skip(1)
 
 lexer = lex.lex()

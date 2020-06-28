36e5568b26a726eb655400bd5cfe8893937bcb23
S. Otzoy <otzoy97@hotmail.com>
2020-06-27 23:04:48 -0600

    changes on primary type

--- interpreter/expression/primary.py
+++ interpreter/expression/primary.py
@@ -2,14 +2,14 @@ from interpreter.instruction import Instruction
 from enum import Enum
 
 class PrimaryType(Enum):
-    FLOATING = 0
-    CHARACTER = 1
-    STRING = 2
-    INTEGER = 3
-    IDENTIFIER = 4
+    CONSTANT = 0
+    IDENTIFIER = 1
 
 class Primary(Instruction):
     def __init__(self, value, type, row):
         self.value = value
         self.type = type
         self.row = row
+
+    def firstRun(self):
+        pass

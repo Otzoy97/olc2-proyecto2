3834c20fc7926a51b7a12aeefc9f1d46d88fc2e3
S. Otzoy <otzoy97@hotmail.com>
2020-06-30 13:30:34 -0600

    added firstRun function to primary class

--- interpreter/expression/primary.py
+++ interpreter/expression/primary.py
@@ -1,4 +1,5 @@
 from interpreter.instruction import Instruction
+from interpreter.quadruple import Quadruple, OperatorQuadruple
 from enum import Enum
 
 class PrimaryType(Enum):
@@ -11,5 +12,14 @@ class Primary(Instruction):
         self.type = type
         self.row = row
 
-    def firstRun(self):
-        pass
+    def firstRun(self, localE):
+        if self.type == 1:
+            #it is an identifier, look for it in the symbols table
+            sym = localE.symTable.find(self.value)
+            if sym != None:
+                #there's a symbol associated with this identificator
+                #return a temporary label
+                return ("symbol", sym)
+        else:
+            #it is a raw value, it returns
+            return ("rawvalue", self.value)

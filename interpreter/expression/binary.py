1e9f087694e035b8bf2e31096296cfad016ec8f5
S. Otzoy <otzoy97@hotmail.com>
2020-07-01 02:45:25 -0600

    add firstRun function

--- interpreter/expression/binary.py
+++ interpreter/expression/binary.py
@@ -10,5 +10,49 @@ class Binary(Instruction):
         self.exp2 = exp2
 
     def firstRun(self, localE):
-        pass
-        
+        arg1 = self.exp1.firstRun(localE)
+        arg2 = self.exp2.firstRun(localE)
+        tempName1 = arg1[1].temp if arg1[0] == "symbol" else arg1[1]
+        tempName2 = arg2[1].temp if arg2[0] == "symbol" else arg2[1]
+        q0 = Quadruple(self.setOperator(), tempName1, tempName2, f"$t{SymbolTable.IdxTempVar}")
+        SymbolTable.IdxTempVar += 1
+        Quadruple.QDict.append(q0)
+        return("tempname", q0.r)
+    
+    def setOperator(self):
+        if self.op == Operator.OR:
+            return OperatorQuadruple.OR
+        elif self.op == Operator.AND:
+            return OperatorQuadruple.AND
+        elif self.op == Operator.ORBW:
+            return OperatorQuadruple.ORBW
+        elif self.op == Operator.XORBW:
+            return OperatorQuadruple.XORBW
+        elif self.op == Operator.ANDBW:
+            return OperatorQuadruple.ANDBW
+        elif self.op == Operator.EQ:
+            return OperatorQuadruple.EQ
+        elif self.op == Operator.NEQ:
+            return OperatorQuadruple.NEQ
+        elif self.op == Operator.LS:
+            return OperatorQuadruple.LS
+        elif self.op == Operator.GR:
+            return OperatorQuadruple.GR
+        elif self.op == Operator.LSE:
+            return OperatorQuadruple.LSE
+        elif self.op == Operator.GRE:
+            return OperatorQuadruple.GRE
+        elif self.op == Operator.SHL:
+            return OperatorQuadruple.SHL
+        elif self.op == Operator.SHR:
+            return OperatorQuadruple.SHR
+        elif self.op == Operator.PLUS:
+            return OperatorQuadruple.PLUS
+        elif self.op == Operator.MINUS:
+            return OperatorQuadruple.MINUS
+        elif self.op == Operator.TIMES:
+            return OperatorQuadruple.TIMES
+        elif self.op == Operator.QUOTIENT:
+            return OperatorQuadruple.QUOTIENT
+        elif self.op == Operator.REMAINDER:
+            return OperatorQuadruple.REMAINDER

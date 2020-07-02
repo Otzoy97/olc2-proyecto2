bd5f0b00ab3310a49962f1206d74cf546b0c48d0
S. Otzoy <otzoy97@hotmail.com>
2020-07-02 16:10:55 -0600

    added functionality to return and goto op

--- interpreter/expression/jump.py
+++ interpreter/expression/jump.py
@@ -1,9 +1,26 @@
 from interpreter.instruction import Instruction
+from interpreter.st import Symbol, SymbolTable
+from interpreter.quadruple import Quadruple, OperatorQuadruple
+from interpreter.operator import Operator
 
 class Jump(Instruction):
     def __init__(self, op, exp):
-        self.op = op
         self.exp = exp
+        self.op = op
 
-    def firstRun(self):
-        pass
+    def firstRun(self, localE):
+        if self.op == Operator.CONTINUE_:
+            pass
+        elif self.op == Operator.BREAK_:
+            pass
+        elif self.op == Operator.RETURN_:
+            if self.exp == None:
+                Quadruple.QDict.append(Quadruple(OperatorQuadruple.GOTO, f"ret0", None, None))
+            else:
+                expName = self.exp.firstRun(localE)
+                expName = expName[1].temp if expName[0] == "symbol"  else expName[1]
+                Quadruple.QDict.append(Quadruple(OperatorQuadruple.ASSIGNMENT, expName, None, "$v0"))
+                return ("rawvalue", 0)
+        elif self.op == Operator.GOTO:
+            Quadruple.QDict.append(Quadruple(OperatorQuadruple.GOTO, self.exp, None, None ))
+            return ("rawvalue", 0)

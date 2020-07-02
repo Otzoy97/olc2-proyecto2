6a261a7999de453120a3968ac642a3dc897f9ac1
S. Otzoy <otzoy97@hotmail.com>
2020-07-02 01:17:34 -0600

    added storage class Program

---
+++ interpreter/environment/Program.py
@@ -0,0 +1,17 @@
+from interpreter.instruction import Instruction
+from interpreter.st import SymbolTable, Symbol, SymbolType
+from interpreter.dspecifier import DSpecifier
+
+class Program(Instruction):
+    def __init__(self):
+        self.sym = SymbolTable()
+        self.ist = []
+
+    def firstRun(self):
+        '''Recorre los arreglos de function y declaration'''
+        for i in self.ist:
+            i.firstRun(self)
+    
+    def findSymbol(self, identifier):
+        '''Busca un simbolo'''
+        return self.sym.find(identifier)

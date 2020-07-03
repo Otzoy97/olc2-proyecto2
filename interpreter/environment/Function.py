3011535af618ed403628fb4766daea81f5ee4dab
S. Otzoy <otzoy97@hotmail.com>
2020-07-03 07:48:04 -0600

    added firstRun function

--- interpreter/environment/Function.py
+++ interpreter/environment/Function.py
@@ -1,19 +1,45 @@
 from interpreter.instruction import Instruction
 from interpreter.st import SymbolTable, Symbol, SymbolType
 from interpreter.dspecifier import DSpecifier
+from interpreter.quadruple import Quadruple, OperatorQuadruple
 
 class Function(Instruction):
-    def __init__(self, decSpec, declarator, cStatement, parent, row):
+    def __init__(self, decSpec, declarator, cStatement, row):
         self.sym = SymbolTable()
-        self.decSpec = decSpec
+        self.decSpec = decSpec #no se usa
         self.dec = declarator
         self.csta = cStatement
-        self.parent = parent
+        self.parent = None
         self.row = row
 
-    def firstRun(self):
-        pass
-
+    def firstRun(self, localE):
+        #establece el entorno padre
+        self.parent = localE
+        #recupera el identificador para la función
+        functionId = self.dec[0]
+        #guarda el sybolo en la tabla padre
+        func = Symbol(functionId, None, SymbolType.FUNCTION, self.parent.retrieveEnvironment(),self.row)
+        func.parlist = self.dec[1]
+        self.parent.sym.addFunction()
+        #almacena el número de parametros
+        noParams = len(self.dec[1])
+        counterParams = 0
+        #recorre los parametros y los guarda en la tabla de simbolos local
+        for param in self.dec[1]:
+            s = Symbol("$s0[$ra]",None,SymbolType.VARIABLE,functionId, self.row)
+            #indica que es una variable de función (local)
+            s.param = True
+            #establece el indice del parametro
+            s.idxParam = noParams - counterParams
+            counterParams += 1
+            #agrega el symbolo a la tabla de simbolos locales
+            self.sym.add(param, s)
+        #ejecuta los statements
+        for sta in self.csta:
+            sta.firstRun(self)
+        #añade un etiqueta de retorno
+        Quadruple.QDict.append(Quadruple(OperatorQuadruple.GOTO, "ret0", None, None))
+        
     def findSymbol(self, identifier):
         '''Busca un simbolo'''
         r = self.sym.find(identifier)

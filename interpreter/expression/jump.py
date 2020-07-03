3ac48949f53756819c5225a70581156384909e56
S. Otzoy <otzoy97@hotmail.com>
2020-07-03 09:19:54 -0600

    fixed return instruction

--- interpreter/expression/jump.py
+++ interpreter/expression/jump.py
@@ -30,6 +30,7 @@ class Jump(Instruction):
                 expName = self.exp.firstRun(localE)
                 expName = expName[1].temp if expName[0] == "symbol"  else expName[1]
                 Quadruple.QDict.append(Quadruple(OperatorQuadruple.ASSIGNMENT, expName, None, "$v0"))
+                Quadruple.QDict.append(Quadruple(OperatorQuadruple.GOTO, f"ret0", None, None))
             return ("rawvalue", 0)
         elif self.op == Operator.GOTO:
             Quadruple.QDict.append(Quadruple(OperatorQuadruple.GOTO, self.exp, None, None ))

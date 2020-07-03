a8600d15e934dfac36e47b3cd936b459176b002b
S. Otzoy <otzoy97@hotmail.com>
2020-07-03 09:20:18 -0600

    some fixed to printf function

--- interpreter/expression/printf.py
+++ interpreter/expression/printf.py
@@ -8,10 +8,16 @@ class Printf(Instruction):
         self.exp = exp
 
     def firstRun(self, localE):
-        baseString = self.exp[0][1:-1]
+        baseString = self.exp[0].firstRun(localE)
+        baseString = baseString[1]
+        if isinstance(baseString, str):
+            baseString = baseString[1:-1]
+        else:
+            return ("rawvalue", 0)
         idx = 1
         i = 0
         if len(self.exp) > 1:
+            #FIXME: no imprime bien cuando hay un caracter de escpe
             while i < len(baseString):
                 if idx < len(self.exp) and baseString[i] == r"%":
                     baseName = self.exp[idx].firstRun(localE)
@@ -20,6 +26,14 @@ class Printf(Instruction):
                     else:
                         Quadruple.QDict.append(Quadruple(OperatorQuadruple.PRINT, baseName[1], None, None))
                     idx += 1
+                    i += 1
+                if idx < len(self.exp) and baseString[i] == r"\\":
+                    #trat ade recuperar el caractr siguiente:
+                    try:
+                        self.nextchar = "\\" +baseString[i+1]
+                    except:
+                        self.nextchar = "\\"
+                    Quadruple.QDict.append(Quadruple(OperatorQuadruple.PRINT, f'"{self.nextchar}"', None, None))
                     i += 1
                 else:
                     Quadruple.QDict.append(Quadruple(OperatorQuadruple.PRINT, f'"{str(baseString[i])}"', None, None))
